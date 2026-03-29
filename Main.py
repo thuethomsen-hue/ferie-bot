753037ef2f7d822746ce107754a20f6d4250ca1e
thuethomsen-hue <thuethomsen@gmail.com>
2026-03-29 10:59:45 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,28 @@
+import requests
+import time
+from playwright.sync_api import sync_playwright
+
+TELEGRAM_TOKEN = "DIT_TOKEN"
+CHAT_ID = "8172803404"
+
+def send(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
+
+def get_price():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://www.apollorejser.dk/spanien/fuerteventura/playitas-resort")
+        page.wait_for_timeout(8000)
+
+        prices = page.locator("text=kr").all_text_contents()
+        browser.close()
+
+        return prices[0] if prices else "Ingen pris fundet"
+
+while True:
+    send(get_price())
+    time.sleep(86400)
